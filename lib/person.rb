2b6f817154178951aa9ae522887ca197e8889391
AstralSol <porphyrios33@gmail.com>
2020-11-08 18:16:41 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :height, :weight, :handed, :complexion,
+    :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)   # gets an sets every attribute on an object instantiated from Person
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
 end
+
+# With this pattern, we have made our code much more flexible.
+ # We can easily alter the number of attributes in the class and 
+ # change the hash that we initialize the class with, without editing our initialize method.
